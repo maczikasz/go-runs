235f402652650f74df087232be4e5cfeea448b22
Maczika Szava, INI-DNA-NB <Szava.Maczika@swisscom.com>
2021-03-25 22:34:09 +0100

    Fixed tests

--- internal/server/servermongo_test.go
+++ internal/server/servermongo_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/maczikasz/go-runs/internal/test_utils"
 	log "github.com/sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -175,243 +174,369 @@ func DoTestHttpServerWithMongo(t *testing.T, client *mongodb.MongoClient) error 
 		return err
 	}
 
-	Convey("Given server set up using mongodb", t, func() {
+	var step1Id string
+	var step2Id string
+	var runbookId string
+	var runbookId2 string
+	var lastSession model.Session
 
-		Convey("When matching message rule is saved", func() {
-			messageMatchingRunbookId := primitive.NewObjectID().Hex()
-			rule := RuleCreateDTO{
-				RuleType:    "message",
-				MatcherType: "equal",
-				RuleContent: "Test message",
-				RunbookId:   messageMatchingRunbookId,
-			}
-			bytes, _ := json2.Marshal(&rule)
+	step1 := stepDTO{
+		Summary: "Test summary",
+		Markdown: MarkdownInfo{
+			Content: "TEST_MARKDOWN",
+			Type:    "gridfs",
+		},
+		Type: "Workaround",
+	}
 
-			r, _ := http.NewRequest(http.MethodPost, "/rules", bytes2.NewReader(bytes))
+	step2 := stepDTO{
+		Summary: "Test summary1",
+		Markdown: MarkdownInfo{
+			Content: "TEST_MARKDOWN1",
+			Type:    "gridfs",
+		},
+		Type: "Workaround1",
+	}
+
+	Convey("Given 2 steps and a runbook is created", t, func() {
+
+		bytes, _ := json2.Marshal(step1)
+
+		r, _ := http.NewRequest(http.MethodPost, "/details", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		step1Id = w.Body.String()
+
+		bytes, _ = json2.Marshal(step2)
+
+		r, _ = http.NewRequest(http.MethodPost, "/details", bytes2.NewReader(bytes))
+		w = httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		step2Id = w.Body.String()
+
+		bytes, _ = json2.Marshal(runbookDTO{Steps: []string{step1Id}})
+
+		r, _ = http.NewRequest(http.MethodPost, "/runbooks", bytes2.NewReader(bytes))
+		w = httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		runbookId = w.Body.String()
+
+		bytes, _ = json2.Marshal(runbookDTO{Steps: []string{step2Id}})
+
+		r, _ = http.NewRequest(http.MethodPost, "/runbooks", bytes2.NewReader(bytes))
+		w = httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		runbookId2 = w.Body.String()
+
+	})
+
+	Convey("Given matching message rule is saved", t, func() {
+		rule := RuleCreateDTO{
+			RuleType:    "message",
+			MatcherType: "equal",
+			RuleContent: "Test message",
+			RunbookId:   runbookId,
+		}
+		bytes, _ := json2.Marshal(&rule)
+
+		r, _ := http.NewRequest(http.MethodPost, "/rules", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusCreated)
+
+		Convey("Then rule list contains name rule", func() {
+			r, _ := http.NewRequest(http.MethodGet, "/rules", bytes2.NewReader(bytes))
 			w := httptest.NewRecorder()
 
 			router.ServeHTTP(w, r)
-			So(w.Code, ShouldEqual, http.StatusCreated)
 
-			Convey("Then rule list contains name rule", func() {
-				r, _ := http.NewRequest(http.MethodGet, "/rules", bytes2.NewReader(bytes))
-				w := httptest.NewRecorder()
+			var rules []model.RuleEntity
 
-				router.ServeHTTP(w, r)
+			body, _ := ioutil.ReadAll(w.Body)
 
-				var rules []model.RuleEntity
+			_ = json2.Unmarshal(body, &rules)
 
-				body, _ := ioutil.ReadAll(w.Body)
+			So(rules, ShouldNotBeEmpty)
+			So(len(rules), ShouldEqual, 1)
 
-				_ = json2.Unmarshal(body, &rules)
+			So(rules, test_utils.ShouldMatch, func(actual interface{}) bool {
+				if matchRule, ok := actual.(model.RuleEntity); ok {
+					matches := true
 
-				So(rules, ShouldNotBeEmpty)
-				So(len(rules), ShouldEqual, 1)
+					matches = matches && matchRule.RunbookId == rule.RunbookId
+					matches = matches && matchRule.RuleContent == rule.RuleContent
+					matches = matches && matchRule.MatcherType == rule.MatcherType
 
-				So(rules, test_utils.ShouldMatch, func(actual interface{}) bool {
-					if matchRule, ok := actual.(model.RuleEntity); ok {
-						matches := true
-
-						matches = matches && matchRule.RunbookId == rule.RunbookId
-						matches = matches && matchRule.RuleContent == rule.RuleContent
-						matches = matches && matchRule.MatcherType == rule.MatcherType
-
-						return matches
-					}
-
-					return false
-				})
-
-			})
-
-			Convey("Then matching error returns session with correct runbookId", func() {
-
-				err := model.Error{
-					Name:    "Test Error 1",
-					Message: "Test message",
-					Tags:    []string{"Tag1", "Tag2"},
+					return matches
 				}
-				bytes, _ := json2.Marshal(err)
-				r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
-				w := httptest.NewRecorder()
 
-				router.ServeHTTP(w, r)
-
-				body, _ := ioutil.ReadAll(w.Body)
-				Convey("Then a sessionId is returned", func() {
-					So(w.Code, ShouldEqual, 200)
-					So(body, ShouldNotBeEmpty)
-				})
-
-				sessionId := string(body)
-
-				Convey("When the sessionId is queried", func() {
-
-					r, _ := http.NewRequest(http.MethodGet, "/sessions/"+sessionId, bytes2.NewReader(bytes))
-					w := httptest.NewRecorder()
-
-					router.ServeHTTP(w, r)
-
-					body, _ := ioutil.ReadAll(w.Body)
-
-					var session model.Session
-					_ = json2.Unmarshal(body, &session)
-
-					Convey("Then the correct runbook id is returned", func() {
-
-						So(w.Code, ShouldEqual, 200)
-						So(session.SessionId, ShouldEqual, sessionId)
-						So(session.Runbook.Id, ShouldEqual, messageMatchingRunbookId)
-					})
-				})
+				return false
 			})
 
-			Convey("Then non-matching error returns 400", func() {
-				err := model.Error{
-					Name:    "Test Error 1",
-					Message: "Different message",
-					Tags:    []string{"Tag1", "Tag2"},
+		})
+	})
+
+	Convey("Then matching error returns lastSession with correct runbookId", t, func() {
+
+		err := model.Error{
+			Name:    "Test Error 1",
+			Message: "Test message",
+			Tags:    []string{"Tag1", "Tag2"},
+		}
+		bytes, _ := json2.Marshal(err)
+		r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		body, _ := ioutil.ReadAll(w.Body)
+		Convey("Then a sessionId is returned", func() {
+			So(w.Code, ShouldEqual, 200)
+			So(body, ShouldNotBeEmpty)
+		})
+
+		sessionId := string(body)
+
+		Convey("When the sessionId is queried", func() {
+
+			r, _ := http.NewRequest(http.MethodGet, "/sessions/"+sessionId, bytes2.NewReader(bytes))
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, r)
+
+			body, _ := ioutil.ReadAll(w.Body)
+
+			_ = json2.Unmarshal(body, &lastSession)
+
+			Convey("Then the correct runbook id is returned", func() {
+
+				So(w.Code, ShouldEqual, 200)
+				So(lastSession.SessionId, ShouldEqual, sessionId)
+				So(lastSession.Runbook.Id, ShouldEqual, runbookId)
+			})
+		})
+	})
+
+	Convey("Then runbook steps could be read back", t, func() {
+
+		r, _ := http.NewRequest(http.MethodGet, "/runbooks/"+lastSession.Runbook.Id, nil)
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		var details model.RunbookDetails
+		body, _ := ioutil.ReadAll(w.Body)
+
+		_ = json2.Unmarshal(body, &details)
+
+		So(details.Steps, test_utils.ShouldMatch, func(value interface{}) bool {
+			if step, ok := value.(model.RunbookStepData); ok {
+				return step.Id == step1Id && step.Summary == step1.Summary && step.Type == step1.Type
+			}
+
+			return false
+		})
+	})
+
+	Convey("Then non-matching error returns 400", t, func() {
+		err := model.Error{
+			Name:    "Test Error 1",
+			Message: "Different message",
+			Tags:    []string{"Tag1", "Tag2"},
+		}
+		bytes, _ := json2.Marshal(err)
+		r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		Convey("Then a sessionId is returned", func() {
+			So(w.Code, ShouldEqual, 400)
+		})
+	})
+
+	Convey("When matching name rule is saved", t, func() {
+
+		rule := RuleCreateDTO{
+			RuleType:    "name",
+			MatcherType: "contains",
+			RuleContent: "name",
+			RunbookId:   runbookId2,
+		}
+		bytes, _ := json2.Marshal(&rule)
+
+		r, _ := http.NewRequest(http.MethodPost, "/rules", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusCreated)
+
+		Convey("Then rule list contains both rules", func() {
+			r, _ := http.NewRequest(http.MethodGet, "/rules", bytes2.NewReader(bytes))
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, r)
+
+			var rules []model.RuleEntity
+
+			body, _ := ioutil.ReadAll(w.Body)
+
+			_ = json2.Unmarshal(body, &rules)
+
+			So(rules, ShouldNotBeEmpty)
+			So(len(rules), ShouldEqual, 2)
+			So(rules, test_utils.ShouldMatch, func(actual interface{}) bool {
+				if matchRule, ok := actual.(model.RuleEntity); ok {
+					matches := true
+
+					matches = matches && matchRule.RunbookId == rule.RunbookId
+					matches = matches && matchRule.RuleContent == rule.RuleContent
+					matches = matches && matchRule.MatcherType == rule.MatcherType
+
+					return matches
 				}
-				bytes, _ := json2.Marshal(err)
-				r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
-				w := httptest.NewRecorder()
 
-				router.ServeHTTP(w, r)
-
-				Convey("Then a sessionId is returned", func() {
-					So(w.Code, ShouldEqual, 400)
-				})
+				return false
 			})
 
-			Convey("When matching name rule is saved", func() {
+		})
+	})
 
-				nameMatchingRunbookId := primitive.NewObjectID().Hex()
-				rule := RuleCreateDTO{
-					RuleType:    "name",
-					MatcherType: "contains",
-					RuleContent: "name",
-					RunbookId:   nameMatchingRunbookId,
-				}
-				bytes, _ := json2.Marshal(&rule)
+	Convey("Then matching error returns lastSession with correct runbookId", t, func() {
+		err := model.Error{
+			Name:    "Test name 1",
+			Message: "Test message",
+			Tags:    []string{"Tag1", "Tag2"},
+		}
+		bytes, _ := json2.Marshal(err)
+		r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
 
-				r, _ := http.NewRequest(http.MethodPost, "/rules", bytes2.NewReader(bytes))
-				w := httptest.NewRecorder()
+		router.ServeHTTP(w, r)
 
-				router.ServeHTTP(w, r)
-				So(w.Code, ShouldEqual, http.StatusCreated)
+		body, _ := ioutil.ReadAll(w.Body)
+		Convey("Then a sessionId is returned", func() {
+			So(w.Code, ShouldEqual, 200)
+			So(body, ShouldNotBeEmpty)
+		})
 
-				Convey("Then rule list contains both rules", func() {
-					r, _ := http.NewRequest(http.MethodGet, "/rules", bytes2.NewReader(bytes))
-					w := httptest.NewRecorder()
+		sessionId := string(body)
 
-					router.ServeHTTP(w, r)
+		Convey("When the sessionId is queried", func() {
 
-					var rules []model.RuleEntity
+			r, _ := http.NewRequest(http.MethodGet, "/sessions/"+sessionId, bytes2.NewReader(bytes))
+			w := httptest.NewRecorder()
 
-					body, _ := ioutil.ReadAll(w.Body)
+			router.ServeHTTP(w, r)
 
-					_ = json2.Unmarshal(body, &rules)
+			body, _ := ioutil.ReadAll(w.Body)
 
-					So(rules, ShouldNotBeEmpty)
-					So(len(rules), ShouldEqual, 2)
-					So(rules, test_utils.ShouldMatch, func(actual interface{}) bool {
-						if matchRule, ok := actual.(model.RuleEntity); ok {
-							matches := true
+			_ = json2.Unmarshal(body, &lastSession)
 
-							matches = matches && matchRule.RunbookId == rule.RunbookId
-							matches = matches && matchRule.RuleContent == rule.RuleContent
-							matches = matches && matchRule.MatcherType == rule.MatcherType
+			Convey("Then the correct runbook id is returned", func() {
 
-							return matches
-						}
-
-						return false
-					})
-
-				})
-
-				Convey("Then matching error returns session with correct runbookId", func() {
-					err := model.Error{
-						Name:    "Test name 1",
-						Message: "Test message",
-						Tags:    []string{"Tag1", "Tag2"},
-					}
-					bytes, _ := json2.Marshal(err)
-					r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
-					w := httptest.NewRecorder()
-
-					router.ServeHTTP(w, r)
-
-					body, _ := ioutil.ReadAll(w.Body)
-					Convey("Then a sessionId is returned", func() {
-						So(w.Code, ShouldEqual, 200)
-						So(body, ShouldNotBeEmpty)
-					})
-
-					sessionId := string(body)
-
-					Convey("When the sessionId is queried", func() {
-
-						r, _ := http.NewRequest(http.MethodGet, "/sessions/"+sessionId, bytes2.NewReader(bytes))
-						w := httptest.NewRecorder()
-
-						router.ServeHTTP(w, r)
-
-						body, _ := ioutil.ReadAll(w.Body)
-
-						var session model.Session
-						_ = json2.Unmarshal(body, &session)
-
-						Convey("Then the correct runbook id is returned", func() {
-
-							So(w.Code, ShouldEqual, 200)
-							So(session.SessionId, ShouldEqual, sessionId)
-							So(session.Runbook.Id, ShouldEqual, nameMatchingRunbookId)
-						})
-					})
-				})
-
-				Convey("Then error matching the message rule returns with correct runbookId", func() {
-					err := model.Error{
-						Name:    "Test Error 1",
-						Message: "Test message",
-						Tags:    []string{"Tag1", "Tag2"},
-					}
-					bytes, _ := json2.Marshal(err)
-					r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
-					w := httptest.NewRecorder()
-
-					router.ServeHTTP(w, r)
-
-					body, _ := ioutil.ReadAll(w.Body)
-					Convey("Then a sessionId is returned", func() {
-						So(w.Code, ShouldEqual, 200)
-						So(body, ShouldNotBeEmpty)
-					})
-
-					sessionId := string(body)
-
-					Convey("When the sessionId is queried", func() {
-
-						r, _ := http.NewRequest(http.MethodGet, "/sessions/"+sessionId, bytes2.NewReader(bytes))
-						w := httptest.NewRecorder()
-
-						router.ServeHTTP(w, r)
-
-						body, _ := ioutil.ReadAll(w.Body)
-
-						var session model.Session
-						_ = json2.Unmarshal(body, &session)
-
-						Convey("Then the correct runbook id is returned", func() {
-
-							So(w.Code, ShouldEqual, 200)
-							So(session.SessionId, ShouldEqual, sessionId)
-							So(session.Runbook.Id, ShouldEqual, messageMatchingRunbookId)
-						})
-					})
-				})
+				So(w.Code, ShouldEqual, 200)
+				So(lastSession.SessionId, ShouldEqual, sessionId)
+				So(lastSession.Runbook.Id, ShouldEqual, runbookId2)
 			})
+		})
+	})
+
+	Convey("Then runbook steps could be read back", t, func() {
+
+		r, _ := http.NewRequest(http.MethodGet, "/runbooks/"+lastSession.Runbook.Id, nil)
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		var details model.RunbookDetails
+		body, _ := ioutil.ReadAll(w.Body)
+
+		_ = json2.Unmarshal(body, &details)
+
+		So(details.Steps, test_utils.ShouldMatch, func(value interface{}) bool {
+			if step, ok := value.(model.RunbookStepData); ok {
+				return step.Id == step2Id && step.Summary == step2.Summary && step.Type == step2.Type
+			}
+
+			return false
+		})
+	})
+
+	Convey("Then error matching the message rule returns with correct runbookId", t, func() {
+		err := model.Error{
+			Name:    "Test Error 1",
+			Message: "Test message",
+			Tags:    []string{"Tag1", "Tag2"},
+		}
+		bytes, _ := json2.Marshal(err)
+		r, _ := http.NewRequest(http.MethodPost, "/errors", bytes2.NewReader(bytes))
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		body, _ := ioutil.ReadAll(w.Body)
+		Convey("Then a sessionId is returned", func() {
+			So(w.Code, ShouldEqual, 200)
+			So(body, ShouldNotBeEmpty)
+		})
+
+		sessionId := string(body)
+
+		Convey("When the sessionId is queried", func() {
+
+			r, _ := http.NewRequest(http.MethodGet, "/sessions/"+sessionId, nil)
+			w := httptest.NewRecorder()
+
+			router.ServeHTTP(w, r)
+
+			body, _ := ioutil.ReadAll(w.Body)
+
+			_ = json2.Unmarshal(body, &lastSession)
+
+			Convey("Then the correct runbook id is returned", func() {
+
+				So(w.Code, ShouldEqual, 200)
+				So(lastSession.SessionId, ShouldEqual, sessionId)
+				So(lastSession.Runbook.Id, ShouldEqual, runbookId)
+			})
+		})
+	})
+
+	Convey("Then runbook steps could be read back", t, func() {
+
+		r, _ := http.NewRequest(http.MethodGet, "/runbooks/"+lastSession.Runbook.Id, nil)
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+		So(w.Code, ShouldEqual, http.StatusOK)
+
+		var details model.RunbookDetails
+		body, _ := ioutil.ReadAll(w.Body)
+
+		_ = json2.Unmarshal(body, &details)
+
+		So(details.Steps, test_utils.ShouldMatch, func(value interface{}) bool {
+			if step, ok := value.(model.RunbookStepData); ok {
+				return step.Id == step1Id && step.Summary == step1.Summary && step.Type == step1.Type
+			}
+
+			return false
 		})
 	})
 
@@ -468,7 +593,7 @@ func initializeRouter(client *mongodb.MongoClient) (*gin.Engine, error) {
 		RuleFinder:               rules2.PersistentRuleReader{Mongo: client},
 		RuleMatcher:              ruleManager,
 		RunbookStepDetailsWriter: stepDetailsWriter,
-		RunbookDetailsWriter: runbookDataManager,
+		RunbookDetailsWriter:     runbookDataManager,
 		RuleReloader: func() {
 
 			config, err := rules2.LoadPriorityRuleConfigFromMongodb(client)
